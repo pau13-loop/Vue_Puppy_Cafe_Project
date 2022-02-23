81a7a5f7e30ad7389c548ee213afcdc35abda7ef
pau13-loop <pllinas@cifpfbmoll.eu>
2022-02-23 23:23:33 +0100

    feat: :sparkles: Set images for home. Make introduction and business philosophy

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,8 +3,8 @@
     <b-container fluid>
       <!-- Header -->
       <b-row class="headerRow">
-        <b-col class="headerColumn">
-          <i class="fa-solid fa-paw iconHome"></i>
+        <b-col class="headerColumn headerColumnTitle">
+          <!-- <i class="fa-solid fa-paw iconHome"></i> -->
           <h3 class="headerTitle">Who we are</h3>
         </b-col>
         <b-col class="headerColumn">
@@ -14,13 +14,151 @@
             ever since the 1500s, when an unknown printer took a galley of type
             and scrambled it to make a type specimen book. It has survived not
             only five centuries, but also the leap into electronic typesetting,
-            remaining essentially unchanged. It was popularised in the 1960s
-            with the release of Letraset sheets containing Lorem Ipsum passages,
-            and more recently with desktop publishing software like Aldus
-            PageMaker including versions of Lorem Ipsum.
+            remaining essentially unchanged.
           </p>
         </b-col>
       </b-row>
+
+      <!-- SECTIONS and IMAGES -->
+      <b-row>
+        <b-col class="columnImgHome columnImgHomeLeft">
+          <img
+            src="../assets/img/home/img-2.jpg"
+            alt="Pets cafe image"
+            class="imageHome"
+          />
+        </b-col>
+        <b-col class="columnTextImage">
+          <div class="containerTextImageRight">
+            <h3 class="headerTextImage textRight">
+              We care for them as much as they care for us too
+            </h3>
+            <p class="paragraphTextImage">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.
+            </p>
+          </div>
+        </b-col>
+      </b-row>
+
+      <b-row class="left">
+        <b-col class="columnTextImage">
+          <div class="containerTextImageLeft">
+            <h3 class="headerTextImage textLeft">
+              Integrity is the foundation of good business, and we value it
+              above all.
+            </h3>
+            <p class="paragraphTextImage">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.
+            </p>
+          </div>
+        </b-col>
+        <b-col class="columnImgHome columnImgHomeRight">
+          <img
+            src="../assets/img/home/img-3.jpg"
+            alt="Pets cafe image"
+            class="imageHome"
+          />
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col class="columnImgHome columnImgHomeLeft">
+          <img
+            src="../assets/img/home/img-6.jpg"
+            alt="Pets cafe image"
+            class="imageHome"
+          />
+        </b-col>
+        <b-col class="columnTextImage">
+          <div class="containerTextImageRight">
+            <h3 class="headerTextImage textRight">
+              How we transformed The Puppy Cafe into a modern space of
+              confidence
+            </h3>
+            <p class="paragraphTextImage">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.
+            </p>
+          </div>
+        </b-col>
+      </b-row>
+
+      <b-row class="left">
+        <b-col class="columnTextImage">
+          <div class="containerTextImageLeft">
+            <h3 class="headerTextImage textLeft">
+              Confidence and trust always will make the best friendships between
+              animal kingdom and human kind
+            </h3>
+            <p class="paragraphTextImage">
+              Lorem Ipsum is simply dummy text of the printing and typesetting
+              industry.Lorem Ipsum is simply dummy text of the printing and
+              typesetting industry. Lorem Ipsum is simply dummy text of the
+              printing and typesetting industry.
+            </p>
+          </div>
+        </b-col>
+        <b-col class="columnImgHome columnImgHomeRight">
+          <img
+            src="../assets/img/home/img-5.jpg"
+            alt="Pets cafe image"
+            class="imageHome"
+          />
+        </b-col>
+      </b-row>
+
+      <!-- 
+      <b-row>
+        <b-col class="columnImgHome columnImgHomeLeft">
+          <img
+            src="../assets/img/home/img-4.jpg"
+            alt="Pets cafe image"
+            class="imageHome"
+          />
+        </b-col>
+        <b-col class="containerTextImage">
+          <h3 class="headerTextImage">
+            How we transformed The Puppy Cafe into a modern space of confidence
+          </h3>
+          <p class="paragraphTextImage">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry.
+          </p>
+        </b-col>
+      </b-row>
+
+      <b-row>
+        <b-col class="containerTextImage">
+          <h3 class="headerTextImage">
+            Confidence and trust is the bae of any relationship and us know how
+            to build it to deliver the best friendships between animal kingdom
+            and human kind
+          </h3>
+          <p class="paragraphTextImage">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.Lorem Ipsum is simply dummy text of the printing and
+            typesetting industry. Lorem Ipsum is simply dummy text of the
+            printing and typesetting industry.
+          </p>
+        </b-col>
+        <b-col class="columnImgHome columnImgHomeRight">
+          <img
+            src="../assets/img/home/img-5.jpg"
+            alt="Pets cafe image"
+            class="imagesHome"
+          />
+        </b-col>
+      </b-row> -->
     </b-container>
   </div>
 </template>
@@ -45,10 +183,12 @@ export default {
 }
 
 .headerColumn {
-  margin: auto;
+  margin-top: 135px;
 }
-.iconHome {
-  font-size: 25px;
+
+.headerColumnTitle {
+  max-width: 35%;
+  padding-left: 50px;
 }
 
 .headerTitle {
@@ -56,6 +196,55 @@ export default {
 }
 
 .textHeader {
-  font-size: 15px;
+  width: 85%;
+  font-size: 50px;
+}
+
+/* IMAGES SECTION */
+
+/* .left {
+  float: left;
+} */
+
+.columnImgHome {
+  max-height: 1000px;
+  max-width: 50%;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  text-align: center;
+}
+
+/* .columnImgHomeLeft {
+  padding-left: 100px;
+} */
+
+.imageHome {
+  height: 100%;
+  border-radius: 10px;
+}
+
+.columnTextImage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.containerTextImageRight {
+  padding-right: 20%;
+}
+
+.containerTextImageLeft {
+  text-align: end;
+  padding-left: 20%;
+}
+
+.headerTextImage {
+  font-size: 80px;
+  font-style: italic;
+  font-weight: bold;
+}
+
+.paragraphTextImage {
+  font-style: italic;
 }
 </style>
