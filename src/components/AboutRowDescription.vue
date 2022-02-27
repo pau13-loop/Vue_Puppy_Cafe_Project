8fc1385f88a44cf77480f86799257716f3f0d1a4
pau13-loop <pllinas@cifpfbmoll.eu>
2022-02-27 17:53:46 +0100

    feat: :sparkles: Implement About reusable component

---
+++ src/components/AboutRowDescription.vue
@@ -0,0 +1,72 @@
+<template>
+  <!-- LEFT DESCRIPTION -->
+  <b-row v-if="isLeft" class="descriptionRow">
+    <b-col
+      ><h1 class="numberColumn">{{ numberSection }}</h1></b-col
+    >
+    <b-col class="descriptionRightColumn">
+      <div>
+        <h3 class="headerDescription">{{ headerSection }}</h3>
+        <p>
+          {{ contextSection }}
+        </p>
+      </div>
+    </b-col>
+  </b-row>
+
+  <!-- RIGHT DESCRIPTION -->
+  <b-row v-else class="descriptionRow">
+    <b-col class="descriptionLeftColumn">
+      <div>
+        <h3 class="headerDescription">{{ headerSection }}</h3>
+        <p>
+          {{ contextSection }}
+        </p>
+      </div>
+    </b-col>
+    <b-col
+      ><h1 class="numberColumn">{{ numberSection }}</h1></b-col
+    >
+  </b-row>
+</template>
+
+<script>
+export default {
+  name: "AboutRowDescription",
+  props: {
+    isLeft: { required: true, type: Boolean },
+    numberSection: { required: true, type: String },
+    headerSection: { required: true, type: String },
+    contextSection: { required: true, type: String },
+  },
+};
+</script>
+
+<style>
+/* Description Block */
+
+.descriptionRow {
+  margin-top: 65px;
+  margin-bottom: 65px;
+}
+
+.numberColumn {
+  text-align: center;
+  font-size: 120px;
+  font-style: italic;
+}
+
+.descriptionLeftColumn {
+  text-align: end;
+  display: inline-block;
+}
+
+.descriptionRightColumn {
+  text-align: start;
+  display: inline-block;
+}
+
+.headerDescription {
+  font-style: italic;
+}
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -57,80 +57,55 @@
         </b-col>
       </b-row>
 
+      <!-- DESCRIPTION COMPONENT SECTION -->
+
       <!-- Description 01 -->
-      <b-row class="descriptionRow">
-        <b-col class="descriptionLeftColumn">
-          <div>
-            <h3 class="headerDescription">Experience</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
+      <AboutRowDescription
+        v-bind:isLeft="false"
+        numberSection="01"
+        headerSection="Experience"
+        contextSection="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting.
-            </p>
-          </div>
-        </b-col>
-        <b-col><h1 class="numberColumn">01</h1></b-col>
-      </b-row>
+              electronic typesetting."
+      />
 
       <!-- Description 02 -->
-      <b-row class="descriptionRow">
-        <b-col><h1 class="numberColumn">02</h1></b-col>
-        <b-col class="descriptionRightColumn">
-          <div>
-            <h3 class="headerDescription">Innovation</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
+      <AboutRowDescription
+        v-bind:isLeft="true"
+        numberSection="02"
+        headerSection="Innovation"
+        contextSection="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting.
-            </p>
-          </div>
-        </b-col>
-      </b-row>
+              electronic typesetting."
+      />
 
       <!-- Description 03 -->
-      <b-row class="descriptionRow">
-        <b-col class="descriptionLeftColumn">
-          <div>
-            <h3 class="headerDescription">Motivation</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
+      <AboutRowDescription
+        v-bind:isLeft="false"
+        numberSection="03"
+        headerSection="Motivation"
+        contextSection="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting.
-            </p>
-          </div>
-        </b-col>
-        <b-col><h1 class="numberColumn">03</h1></b-col>
-      </b-row>
+              electronic typesetting."
+      />
 
       <!-- Description 04 -->
-      <b-row class="descriptionRow">
-        <b-col><h1 class="numberColumn">04</h1></b-col>
-        <b-col class="descriptionRightColumn">
-          <div>
-            <h3 class="headerDescription">Passion</h3>
-            <p>
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry. Lorem Ipsum has been the industry's standard dummy text
-              ever since the 1500s, when an unknown printer took a galley of
+      <AboutRowDescription
+        v-bind:isLeft="true"
+        numberSection="04"
+        headerSection="Passion"
+        contextSection=" Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of
               type and scrambled it to make a type specimen book. It has
               survived not only five centuries, but also the leap into
-              electronic typesetting.
-            </p>
-          </div>
-        </b-col>
-      </b-row>
+              electronic typesetting."
+      />
+
+      <!-- END DESCRIPTION COMPONENT SECTION -->
 
       <!-- Image -->
-
       <b-row>
         <b-col class="columnImg">
           <div class="imgOne">
@@ -170,7 +145,9 @@
 <script>
 export default {
   name: "About",
-  components: {},
+  components: {
+    AboutRowDescription: () => import("@/components/AboutRowDescription.vue"),
+  },
 };
 </script>
 
@@ -219,33 +196,6 @@ export default {
 
 .sloganSecondParagraph {
   margin-top: 45px;
-}
-
-/* Description Block */
-
-.descriptionRow {
-  margin-top: 65px;
-  margin-bottom: 65px;
-}
-
-.numberColumn {
-  text-align: center;
-  font-size: 120px;
-  font-style: italic;
-}
-
-.descriptionLeftColumn {
-  text-align: end;
-  display: inline-block;
-}
-
-.descriptionRightColumn {
-  text-align: start;
-  display: inline-block;
-}
-
-.headerDescription {
-  font-style: italic;
 }
 
 /* Contact us */
