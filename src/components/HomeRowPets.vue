a8c37e7dadfd3205af7bf83e565ec72b6033a4fe
pau13-loop <pllinas@cifpfbmoll.eu>
2022-02-27 14:40:30 +0100

    feat: :sparkles: Implement component to Home View

--- src/components/HomeRowPets.vue
+++ src/components/HomeRowPets.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- IF TRUE -->
-  <b-row v-if="positionRow" class="rowImagesHome">
-    <b-col class="columnImgHome columnImgHomeLeft">
+  <b-row v-if="isLeft" class="rowImagesHome">
+    <b-col class="columnImgHome">
       <img
         :src="require(`@/assets/img/home/${imagePath}`)"
         alt="Pets cafe image"
@@ -10,7 +10,7 @@
     </b-col>
     <b-col class="columnTextImage">
       <div class="containerTextImageRight">
-        <h3 class="headerTextImage textRight">
+        <h3 class="headerTextImage">
           {{ headerText }}
         </h3>
         <p class="paragraphTextImage">
@@ -21,10 +21,10 @@
   </b-row>
 
   <!-- ELSE -->
-  <b-row v-else class="left rowImagesHome">
+  <b-row v-else class="rowImagesHome">
     <b-col class="columnTextImage">
       <div class="containerTextImageLeft">
-        <h3 class="headerTextImage textLeft">
+        <h3 class="headerTextImage">
           {{ headerText }}
         </h3>
         <p class="paragraphTextImage">
@@ -32,7 +32,7 @@
         </p>
       </div>
     </b-col>
-    <b-col class="columnImgHome columnImgHomeRight">
+    <b-col class="columnImgHome">
       <img
         :src="require(`@/assets/img/home/${imagePath}`)"
         alt="Pets cafe image"
@@ -53,3 +53,61 @@ export default {
   },
 };
 </script>
+
+<style>
+/* IMAGES SECTION */
+
+.rowImagesHome {
+  column-gap: 35px;
+  position: relative;
+  /* animation: mymove 5s;
+  animation-timing-function: ease-in; */
+}
+
+/* @keyframes mymove {
+  from {
+    left: -300px;
+  }
+  to {
+    left: 0px;
+  }
+} */
+
+.columnImgHome {
+  max-height: 120vh;
+  max-width: 50%;
+  margin-top: 50px;
+  margin-bottom: 50px;
+  text-align: center;
+}
+
+.imageHome {
+  height: 100%;
+  border-radius: 10px;
+}
+
+.columnTextImage {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+
+.containerTextImageRight {
+  padding-right: 20%;
+}
+
+.containerTextImageLeft {
+  text-align: end;
+  padding-left: 20%;
+}
+
+.headerTextImage {
+  font-size: 70px;
+  font-style: italic;
+  font-weight: bold;
+}
+
+.paragraphTextImage {
+  font-style: italic;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -20,154 +20,52 @@
       </b-row>
 
       <!-- SECTIONS and IMAGES -->
-      <b-row class="rowImagesHome">
-        <b-col class="columnImgHome columnImgHomeLeft">
-          <img
-            src="../assets/img/home/img-2.jpg"
-            alt="Pets cafe image"
-            class="imageHome"
-          />
-        </b-col>
-        <b-col class="columnTextImage">
-          <div class="containerTextImageRight">
-            <h3 class="headerTextImage textRight">
-              We care for them as much as they care for us too
-            </h3>
-            <p class="paragraphTextImage">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
-              industry.Lorem Ipsum is simply dummy text of the printing and
-              typesetting industry. Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry.
-            </p>
-          </div>
-        </b-col>
-      </b-row>
 
+      <!-- IMG 1 -->
       <HomeRowPets
         v-bind:isLeft="true"
         imagePath="img-2.jpg"
         headerText="We care for them as much as they care for us too"
-        contextText=" Lorem Ipsum is simply dummy text of the printing and typesetting
+        contextText="Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum is simply dummy text of the
               printing and typesetting industry."
       />
 
-      <!-- <b-row class="left rowImagesHome">
-        <b-col class="columnTextImage">
-          <div class="containerTextImageLeft">
-            <h3 class="headerTextImage textLeft">
-              Integrity is the foundation of good business, and we value it
-              above all.
-            </h3>
-            <p class="paragraphTextImage">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+      <!-- IMG 2 -->
+      <HomeRowPets
+        v-bind:isLeft="false"
+        imagePath="img-3.jpg"
+        headerText="Integrity is the foundation of good business, and we value it
+              above all"
+        contextText="Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry.
-            </p>
-          </div>
-        </b-col>
-        <b-col class="columnImgHome columnImgHomeRight">
-          <img
-            src="../assets/img/home/img-3.jpg"
-            alt="Pets cafe image"
-            class="imageHome"
-          />
-        </b-col>
-      </b-row>
+              printing and typesetting industry."
+      />
 
-      <b-row class="rowImagesHome">
-        <b-col class="columnImgHome columnImgHomeLeft">
-          <img
-            src="../assets/img/home/img-6.jpg"
-            alt="Pets cafe image"
-            class="imageHome"
-          />
-        </b-col>
-        <b-col class="columnTextImage">
-          <div class="containerTextImageRight">
-            <h3 class="headerTextImage textRight">
-              How we transformed The Puppy Cafe into a modern space of
-              confidence
-            </h3>
-            <p class="paragraphTextImage">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+      <!-- IMG 3 -->
+      <HomeRowPets
+        v-bind:isLeft="true"
+        imagePath="img-6.jpg"
+        headerText="How we transformed The Puppy Cafe into a modern space of
+              confidence"
+        contextText="Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry.
-            </p>
-          </div>
-        </b-col>
-      </b-row>
+              printing and typesetting industry."
+      />
 
-      <b-row class="left rowImagesHome">
-        <b-col class="columnTextImage">
-          <div class="containerTextImageLeft">
-            <h3 class="headerTextImage textLeft">
-              Confidence and trust always will make the best friendships
-            </h3>
-            <p class="paragraphTextImage">
-              Lorem Ipsum is simply dummy text of the printing and typesetting
+      <!-- IMG 4 -->
+      <HomeRowPets
+        v-bind:isLeft="false"
+        imagePath="img-5.jpg"
+        headerText="Confidence and trust always will make the best friendships"
+        contextText="Lorem Ipsum is simply dummy text of the printing and typesetting
               industry.Lorem Ipsum is simply dummy text of the printing and
               typesetting industry. Lorem Ipsum is simply dummy text of the
-              printing and typesetting industry.
-            </p>
-          </div>
-        </b-col>
-        <b-col class="columnImgHome columnImgHomeRight">
-          <img
-            src="../assets/img/home/img-5.jpg"
-            alt="Pets cafe image"
-            class="imageHome"
-          />
-        </b-col>
-      </b-row> -->
-
-      <!-- 
-      <b-row>
-        <b-col class="columnImgHome columnImgHomeLeft">
-          <img
-            src="../assets/img/home/img-4.jpg"
-            alt="Pets cafe image"
-            class="imageHome"
-          />
-        </b-col>
-        <b-col class="containerTextImage">
-          <h3 class="headerTextImage">
-            How we transformed The Puppy Cafe into a modern space of confidence
-          </h3>
-          <p class="paragraphTextImage">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry.
-          </p>
-        </b-col>
-      </b-row>
-
-      <b-row>
-        <b-col class="containerTextImage">
-          <h3 class="headerTextImage">
-            Confidence and trust is the bae of any relationship and us know how
-            to build it to deliver the best friendships between animal kingdom
-            and human kind
-          </h3>
-          <p class="paragraphTextImage">
-            Lorem Ipsum is simply dummy text of the printing and typesetting
-            industry.Lorem Ipsum is simply dummy text of the printing and
-            typesetting industry. Lorem Ipsum is simply dummy text of the
-            printing and typesetting industry.
-          </p>
-        </b-col>
-        <b-col class="columnImgHome columnImgHomeRight">
-          <img
-            src="../assets/img/home/img-5.jpg"
-            alt="Pets cafe image"
-            class="imagesHome"
-          />
-        </b-col>
-      </b-row> -->
+              printing and typesetting industry."
+      />
     </b-container>
   </div>
 </template>
@@ -177,8 +75,6 @@ export default {
   name: "Home",
   components: {
     HomeRowPets: () => import("@/components/HomeRowPets.vue"),
-    // @ is an alias to /src
-    // HomeContent: () => import("@/components/home/HomeContent.vue"),
   },
 };
 </script>
@@ -208,69 +104,5 @@ export default {
 .textHeader {
   width: 85%;
   font-size: 30px;
-}
-
-/* IMAGES SECTION */
-
-/* .left {
-  float: left;
-} */
-
-.rowImagesHome {
-  column-gap: 35px;
-  position: relative;
-  animation: mymove 5s;
-  animation-timing-function: ease-in;
-}
-
-@keyframes mymove {
-  from {
-    left: -300px;
-  }
-  to {
-    left: 0px;
-  }
-}
-
-.columnImgHome {
-  max-height: 1000px;
-  max-width: 50%;
-  margin-top: 50px;
-  margin-bottom: 50px;
-  text-align: center;
-}
-
-/* .columnImgHomeLeft {
-  padding-left: 100px;
-} */
-
-.imageHome {
-  height: 100%;
-  border-radius: 10px;
-}
-
-.columnTextImage {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-}
-
-.containerTextImageRight {
-  padding-right: 20%;
-}
-
-.containerTextImageLeft {
-  text-align: end;
-  padding-left: 20%;
-}
-
-.headerTextImage {
-  font-size: 80px;
-  font-style: italic;
-  font-weight: bold;
-}
-
-.paragraphTextImage {
-  font-style: italic;
 }
 </style>
